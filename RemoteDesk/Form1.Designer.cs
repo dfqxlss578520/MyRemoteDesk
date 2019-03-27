835ab0bb7d47b716e46337140b3b6a7b941fdff8
dfqxlss578520 <437258247@qq.com>
2019-03-28 01:21:48 +0800

    添加项目文件。

---
+++ RemoteDesk/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace RemoteDesk
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.axMsTscAxNotSafeForScripting1 = new AxMSTSCLib.AxMsTscAxNotSafeForScripting();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtServerIP = new System.Windows.Forms.TextBox();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnBrokenConnect = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axMsTscAxNotSafeForScripting1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axMsTscAxNotSafeForScripting1
+            // 
+            this.axMsTscAxNotSafeForScripting1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.axMsTscAxNotSafeForScripting1.Enabled = true;
+            this.axMsTscAxNotSafeForScripting1.Location = new System.Drawing.Point(0, -12);
+            this.axMsTscAxNotSafeForScripting1.Name = "axMsTscAxNotSafeForScripting1";
+            this.axMsTscAxNotSafeForScripting1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axMsTscAxNotSafeForScripting1.OcxState")));
+            this.axMsTscAxNotSafeForScripting1.Size = new System.Drawing.Size(889, 470);
+            this.axMsTscAxNotSafeForScripting1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 476);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "服务器ip";
+            // 
+            // txtServerIP
+            // 
+            this.txtServerIP.Location = new System.Drawing.Point(69, 470);
+            this.txtServerIP.Name = "txtServerIP";
+            this.txtServerIP.Size = new System.Drawing.Size(100, 21);
+            this.txtServerIP.TabIndex = 2;
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(233, 471);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(100, 21);
+            this.txtUserName.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(190, 476);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "用户名";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(397, 469);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 21);
+            this.txtPassword.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(359, 475);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "密 码";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(533, 471);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 7;
+            this.btnConnect.Text = "连 接";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnBrokenConnect
+            // 
+            this.btnBrokenConnect.Location = new System.Drawing.Point(614, 471);
+            this.btnBrokenConnect.Name = "btnBrokenConnect";
+            this.btnBrokenConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnBrokenConnect.TabIndex = 8;
+            this.btnBrokenConnect.Text = "断开连接";
+            this.btnBrokenConnect.UseVisualStyleBackColor = true;
+            this.btnBrokenConnect.Click += new System.EventHandler(this.btnBrokenConnect_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 499);
+            this.Controls.Add(this.btnBrokenConnect);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtServerIP);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.axMsTscAxNotSafeForScripting1);
+            this.Name = "frmMain";
+            this.Text = "主界面";
+            ((System.ComponentModel.ISupportInitialize)(this.axMsTscAxNotSafeForScripting1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxMSTSCLib.AxMsTscAxNotSafeForScripting axMsTscAxNotSafeForScripting1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtServerIP;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnBrokenConnect;
+    }
+}
+
---
+++ RemoteDesk/Form1.cs
@@ -0,0 +1,65 @@
+﻿using MSTSCLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RemoteDesk
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            string serverip = txtServerIP.Text;
+            string username = txtUserName.Text;
+            string password = txtPassword.Text;
+
+            if(string.IsNullOrEmpty(serverip))
+            {
+                MessageBox.Show("服务器IP不能为空！", "温馨提示");
+                return ;
+            }
+            if (string.IsNullOrEmpty(username))
+            {
+                MessageBox.Show("用户名不能为空！", "温馨提示");
+                return;
+            }
+            if (string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("密码不能为空！", "温馨提示");
+                return;
+            }
+            try
+            {
+                axMsTscAxNotSafeForScripting1.Server = txtServerIP.Text;
+                axMsTscAxNotSafeForScripting1.UserName = txtUserName.Text;
+                IMsTscNonScriptable secured = (IMsTscNonScriptable)axMsTscAxNotSafeForScripting1.GetOcx();
+                secured.ClearTextPassword = txtPassword.Text;
+                axMsTscAxNotSafeForScripting1.Connect();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "温馨提示");
+            }
+        }
+
+        private void btnBrokenConnect_Click(object sender, EventArgs e)
+        {
+            if(axMsTscAxNotSafeForScripting1.Connected.ToString()=="1")
+            {
+                axMsTscAxNotSafeForScripting1.Disconnect();
+            }
+        }
+    }
+}
